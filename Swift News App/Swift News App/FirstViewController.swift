3009653d690405985054ddf71adf10f928431377
Paul <serbpd@gmail.com>
2019-04-23 19:02:49 -0400

    initial commit

---
+++ Swift News App/Swift News App/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  Swift News App
+//
+//  Created by Paul on 4/23/19.
+//  Copyright © 2019 Paul. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ Swift News App/Swift News App/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  Swift News App
+//
+//  Created by Paul on 4/23/19.
+//  Copyright © 2019 Paul. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
