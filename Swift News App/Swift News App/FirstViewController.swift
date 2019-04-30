a82dc4b0a013121b5807087dec18c09d686332cc
Paul <serbpd@gmail.com>
2019-04-29 22:48:34 -0400

    articles retrieving

--- Swift News App/Swift News App/FirstViewController.swift
+++ Swift News App/Swift News App/FirstViewController.swift
@@ -8,13 +8,66 @@
 
 import UIKit
 
-class FirstViewController: UIViewController {
-
+class FirstViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
+    @IBOutlet weak var tableView: UITableView!
+    var articles: [Article] = []
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        tableView.delegate = self
+        tableView.dataSource = self
+        
+        JSONParser.shared.getArticles({result in
+            self.articles = result
+            DispatchQueue.main.async {
+                self.tableView.reloadData()
+            }
+        })
     }
 
-
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return articles.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "articleCell") as! ArticleCell
+        cell.title.text = articles[indexPath.item].title
+        
+        guard let imgURL = URL(string: articles[indexPath.item].imgURL ?? "") else { return cell }
+        URLSession.shared.dataTask(with: imgURL, completionHandler: { data,_,_ in
+            let img = UIImage(data: data!)
+            DispatchQueue.main.async {
+                cell.img.image = img
+            }
+        }).resume()
+        
+        return cell
+    }
 }
 
+class ArticleCell: UITableViewCell {
+    @IBOutlet weak var img: UIImageView!
+    @IBOutlet weak var title: UILabel!
+}
+
+class Article {
+    var publisher: String?
+    var author: String?
+    var title: String?
+    var descr: String?
+    var imgURL: String?
+    var date: Date?
+    var content: String?
+    
+    public init(pub: String, auth: String, title: String, descr: String, img: String, date: String, content: String) {
+        self.publisher = pub
+        self.author = auth
+        self.title = title
+        self.descr = descr
+        self.imgURL = img
+        self.date = Date()
+        self.content = content
+    }
+}
---
+++ Swift News App/Swift News App/JSONParser.swift
@@ -0,0 +1,48 @@
+//
+//  JSONParser.swift
+//  Swift News App
+//
+//  Created by Paul on 4/24/19.
+//  Copyright © 2019 Paul. All rights reserved.
+//
+
+import Foundation
+
+class JSONParser {
+    public static let shared = JSONParser()
+    private var resultArticles: [Article] = []
+    
+    public func getArticles(query: String = "", country: String = "", cat: String = "", source: String = "", sort: String = "", _ completion: @escaping([Article]) -> Void) {
+        var request = "https://newsapi.org/v2/top-headlines?country=us"
+        let session = URLSession.shared
+        
+        request.append("&apiKey=9027771a2bb24ecaad4faa45cf46cd96")
+        
+        guard let url = URL(string: request) else { return }
+        let task = session.dataTask(with: url, completionHandler: { (data, response, error) in
+            guard error == nil else {
+                print(error!.localizedDescription)
+                return
+            }
+            guard let jsonResult = try? JSONSerialization.jsonObject(with: data!, options: []) as? [String: Any], let jsonResponse = jsonResult, let articles = jsonResponse["articles"] as? [[String: Any]] else {
+                print("error in JSONSerialization")
+                return
+            }
+            
+            for article in articles {
+                let author = article["author"] as! String
+                let title = article["title"] as! String
+                let descr = article["description"] as! String
+                let imgURL = article["urlToImage"] as! String
+                let date = article["publishedAt"] as! String
+                let content = article["content"] as! String
+                let s = article["source"] as! [String: Any]
+                let source = s["name"] as! String
+                let a = Article(pub: source, auth: author, title: title, descr: descr, img: imgURL, date: date, content: content)
+                self.resultArticles.append(a)
+            }
+            completion(self.resultArticles)
+        })
+        task.resume()
+    }
+}
